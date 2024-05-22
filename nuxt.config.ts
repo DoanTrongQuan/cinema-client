898a2ed480fea0e34ced22a99955a867c184d0bb
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-05-22 15:17:13 +0700

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,18 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  imports: {
+    dirs: [
+      // Scan top-level modules
+      'composables',
+      // ... or scan modules nested one level deep with a specific name and file extension
+      'composables/*/index.{ts,js,mjs,mts}',
+      // ... or scan all modules within given directory
+      'composables/**',
+      'utils/**',
+      'utils',
+    ],
+  },
 
 
   modules: [
